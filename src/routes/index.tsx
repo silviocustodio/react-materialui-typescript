771cd26e57f15d8bfe168526eab48a20023a0f4e
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2022-12-05 10:34:07 +0000

    feat: Added light theme and provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/start-page" element={<p>Start page</p>} />
+      <Route
+        path="/start-page"
+        element={
+          <Button variant="contained" color="primary">
+            Test
+          </Button>
+        }
+      />
+      {/* <Route path="/start-page" element={<p>Start page</p>} /> */}
       <Route path="*" element={<Navigate to="/start-page" />} />
     </Routes>
   );
