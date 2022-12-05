d25363240daa0cad15d8e2d431e90f433652ddce
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2022-12-05 11:51:53 +0000

    feat: Added dark theme and change theme button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
   return (
     <Routes>
       <Route
         path="/start-page"
         element={
-          <Button variant="contained" color="primary">
-            Test
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Toggle Theme
           </Button>
         }
       />
