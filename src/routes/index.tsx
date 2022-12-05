5fae4a0454d4560d081e296638793225e5a0db06
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2022-12-05 09:51:25 +0000

    feat: Added react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/start-page" element={<p>Start page</p>} />
+      <Route path="*" element={<Navigate to="/start-page" />} />
+    </Routes>
+  );
+};
